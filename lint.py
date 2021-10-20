6351022c8d45c1ba4d9abf30838557f76edcb8f3
Darshan Yenbhar <66029648+darshands184@users.noreply.github.com>
2021-10-20 16:57:00 +0530

    Create lint.py

---
+++ lint.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+#lint.py
+
+
+import sys
+from pylint import lint  
+
+THRESHOLD = -7
+
+run = lint.Run(["factorial.py"], do_exit=False) 
+score = run.linter.stats["global_note"]  
+
+if score < THRESHOLD: 
+    print("Linter failed: Score < threshold value") 
+    sys.exit(1) 
+
+sys.exit(0) 
+
+
+
+# In[ ]:
+
+
+
+
